eaa0283603b3868037135cec6faf3c739ce90c51
Rajneesh058 <114851821+Rajneesh058@users.noreply.github.com>
2023-06-01 22:46:10 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -74,9 +74,7 @@ async def start(client, message):
             try:
                 kk, file_id = message.command[1].split("_", 1)
                 pre = 'checksubp' if kk == 'filep' else 'checksub' 
-                btn.append([InlineKeyboardButton(" 🔄 Try Again", callback_data=f"{pre}#{file_id}")])
-            except (IndexError, ValueError):
-                btn.append([InlineKeyboardButton(" 🔄 Try Again", url=f"https://t.me/{temp.U_NAME}?start={message.command[1]}")])
+                reply_markup=InlineKeyboardMarkup(btn)
         await client.send_message(
             chat_id=message.from_user.id,
             text=FORCE_SUB_TEXT,
