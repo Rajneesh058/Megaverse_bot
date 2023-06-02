d9a5dafc6096797c210b4e875d5612d11c7a2d1b
Rajneesh058 <114851821+Rajneesh058@users.noreply.github.com>
2023-06-02 23:36:03 +0530

    Update welcome.py

--- plugins/welcome.py
+++ plugins/welcome.py
@@ -5,16 +5,16 @@ from info import WELCOM_TEXT, BYE_TEXT
 
 @Client.on_message(filters.new_chat_members)
 async def welcome(bot,message):
-	chatid= message.chat.id	
-	await bot.send_message(text=WELCOM_TEXT
+	chatid= message.chat.id,	
+	await bot.send_message(text=WELCOM_TEXT,
 	reply_markup=[InlineKeyboardButton('BOT CHANNEL', url='https://t.me/Epic_creation_bots'),
             InlineKeyboardButton('UPDATES', url='https://t.me/Movie_Megaverse_Backup')],
             parse_mode=enums.ParseMode.DEFAULT
             )
 @Client.on_message(filters.left_chat_member)
 async def goodbye(bot,message):
-	chatid= message.chat.id
-	await bot.send_message(text=BYE_TEXT
+	chatid= message.chat.id,
+	await bot.send_message(text=BYE_TEXT,
 	reply_markup=[InlineKeyboardButton('BOT CHANNEL', url='https://t.me/Epic_creation_bots'),
             InlineKeyboardButton('UPDATES', url='https://t.me/Movie_Megaverse_Backup')],
             parse_mode=enums.ParseMode.DEFAULT
