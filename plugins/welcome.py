da91e947d43ff7014aea1e36c56072851c4dd678
Rajneesh058 <114851821+Rajneesh058@users.noreply.github.com>
2023-06-04 22:14:07 +0530

    Update welcome.py

--- plugins/welcome.py
+++ plugins/welcome.py
@@ -1,21 +1,12 @@
 import os
 from pyrogram import Client, filters
-from pyrogram.types import Message, User,InlineKeyboardButton,InlineKeyboardMarkup
-from info import WELCOM_TEXT, BYE_TEXT
-
+from pyrogram.types import Message, User
 @Client.on_message(filters.new_chat_members)
 async def welcome(bot,message):
-	chatid= message.chat.id,
-	await bot.send_message(text=WELCOM_TEXT,
-	reply_markup=[InlineKeyboardButton('BOT CHANNEL', url='https://t.me/Epic_creation_bots'),
-            InlineKeyboardButton('UPDATES', url='https://t.me/Movie_Megaverse_Backup')],
-            parse_mode=enums.ParseMode.DEFAULT
-            )
+	chatid= message.chat.id
+	await bot.send_message(text=f"Welcome {message.from_user.mention} to {message.chat.username} ,  Happy to have here",chat_id=chatid)
+	
 @Client.on_message(filters.left_chat_member)
 async def goodbye(bot,message):
-	chatid= message.chat.id,
-	await bot.send_message(text=BYE_TEXT,
-	reply_markup=[InlineKeyboardButton('BOT CHANNEL', url='https://t.me/Epic_creation_bots'),
-            InlineKeyboardButton('UPDATES', url='https://t.me/Movie_Megaverse_Backup')],
-            parse_mode=enums.ParseMode.DEFAULT
-            )
+	chatid= message.chat.id
+	await bot.send_message(text=f"Bye 👋,  {message.from_user.mention} , Have a Nice Day",chat_id=chatid)
