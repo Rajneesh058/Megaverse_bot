f74af9291d1e826fe6df31b78709136609f3b90b
Rajneesh058 <114851821+Rajneesh058@users.noreply.github.com>
2023-06-04 22:49:17 +0530

    Update welcome.py

--- plugins/welcome.py
+++ plugins/welcome.py
@@ -1,12 +1,14 @@
 import os
 from pyrogram import Client, filters
 from pyrogram.types import Message, User
+from info import WELCOM_TEXT, BYE_TEXT
+
 @Client.on_message(filters.new_chat_members)
 async def welcome(bot,message):
 	chatid= message.chat.id
-	await bot.send_message(text=f"Welcome {message.from_user.mention} to {message.chat.username} ,  Happy to have here",chat_id=chatid)
+	await bot.send_message(text=WELCOM_TEXT)
 	
 @Client.on_message(filters.left_chat_member)
 async def goodbye(bot,message):
 	chatid= message.chat.id
-	await bot.send_message(text=f"Bye 👋,  {message.from_user.mention} , Have a Nice Day",chat_id=chatid)
+	await bot.send_message(text=BYE_TEXT)
